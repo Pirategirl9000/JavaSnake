46e8ae20e39132de7c0ac67463901fd5b3a2b949
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2025-08-14 14:59:43 -0500

    Started work on basic getters and setters for the Food class

---
+++ src/Game/Snake/Food.java
@@ -0,0 +1,91 @@
+package Game.Snake;
+
+import java.util.Random;
+
+/**
+ * Holds information regarding the food that the snake eats to grow
+ */
+public class Food {
+    /**
+     * Stores the x and y coordinates of the food
+     */
+    private int[] location;
+
+    /**
+     * Width of the game area, used for generating location of food
+     */
+    private final int DISPLAYWIDTH;
+
+    /**
+     * Height of the game area, used for generating location of food
+     */
+    private final int DISPLAYHEIGHT;
+
+    /**
+     * Width of a food object
+     */
+    private final int FOODWIDTH;
+
+    /**
+     * Height of a food object
+     */
+    private final int FOODHEIGHT;
+
+    /**
+     * The random number generator for the food locations
+     */
+    private final Random rand = new Random();
+
+    public Food(int displayWidth, int displayHeight, int foodWidth, int foodHeight) {
+        rand.setSeed(System.currentTimeMillis());  // Generate seed based on current time
+        DISPLAYWIDTH = displayWidth;
+        DISPLAYHEIGHT = displayHeight;
+        FOODWIDTH = foodWidth;
+        FOODHEIGHT = foodHeight;
+    }
+
+    /**
+     * Returns the location of the food as an array
+     * @return [x, y]
+     */
+    public int[] getLocation() { return location; }
+
+    /**
+     * Returns the x position of the food
+     * @return x-position
+     */
+    public int getX() { return location[0]; }
+
+    /**
+     * Returns the y position of the food
+     * @return y-position
+     */
+    public int getY() { return location[1]; }
+
+    /**
+     * Returns the width of a food object
+     * @return width
+     */
+    public int getFoodWidth() { return FOODWIDTH; }
+
+    /**
+     * Returns the height of a food object
+     * @return height
+     */
+    public int getFoodHeight() { return FOODHEIGHT; }
+
+    /**
+     * Spawns a new food square at a random location
+     */
+    public void generateNewFood() {
+
+    }
+
+    /**
+     * Generates a new seed based on the current time
+     */
+    public void generateNewSeed() { rand.setSeed(System.currentTimeMillis()); }
+
+
+
+}
