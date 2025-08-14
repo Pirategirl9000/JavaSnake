805d3fc404a569eb1827f8049069c96b69210992
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2025-08-13 21:42:30 -0500

    Context comments

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import Game.GameController;
 
 public class Main {
     public static void main(String[] args) {
-        Display display = new Display();
+        Display display = new Display();  // Boot up the game
 
     }
 }
